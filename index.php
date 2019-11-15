a94cca1c216465e14cb43ff2f42da7980da33875
Reynaldo  Morillo <reynaldomorillo.rey@gmail.com>
2019-11-15 14:07:26 -0500

    Encabezado 1

--- index.php
+++ index.php
@@ -2,22 +2,105 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Agencia Turismo</title>
+
+	<link rel="icon" href="img/icono.png">
+
+		<!--=====================================
+		Estilos CSS
+		======================================-->
+				
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Chewy|Open+Sans&display=swap" rel="stylesheet">
+
+	<!--=====================================
+		JS
+		======================================-->
 
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 	<!-- Popper JS -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	
+	<!--Libreria de íconos-->
+	<script src="https://kit.fontawesome.com/534db4e6c6.js" crossorigin="anonymous"></script>
 
 </head>
+
 <body>
 	
+	<!--=====================================
+		CABECERA
+		======================================-->
+	<header class="container-fluid bg-primary">
+		<div class="container bg-danger">
+			<div class="row">
+
+				<!-- LOGO -->
+				<div class="col-7 bg-success">
+					<a href="index.php">
+						<img src="img/logotipo.png" alt="" class="img-fluid logotipo">
+					</a>
+				</div>
+
+				<!-- REDES SOCIALES -->
+				<div class="col-4 bg-warning redes">
+					<ul>
+						<li>
+							<a href="#" target="_blank">
+								<i class="fab fa-facebook-f lead float-left rounded-circle text-white mr-1">
+									
+								</i>
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank">
+								<i class="fab fa-instagram lead float-left rounded-circle text-white mr-1">
+									
+								</i>
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank">
+								<i class="fab fa-twitter lead float-left rounded-circle text-white mr-1">
+									
+								</i>
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank">
+								<i class="fab fa-youtube lead float-left rounded-circle text-white mr-1">
+									
+								</i>
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<!-- BUSCADOR Y BOTÓN MENÚ -->
+				<div class="col-1 bg-danger">
+					<!-- Buscador-->
+					<div class="float-left">
+						<i class="fas fa-search"></i>
+					</div>
+					<!-- Botón Menú-->
+					<div class="float-left">
+						<i class="fas fa-bars"></i>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</header>
+
+<script type="js/script.js"></script>
 </body>
 </html>
+
