102113457d2f4299bd68596e30763fb761f14028
Reynaldo  Morillo <reynaldomorillo.rey@gmail.com>
2019-11-15 22:43:12 -0500

    responsibe-encabezado

--- index.php
+++ index.php
@@ -39,44 +39,44 @@
 	<!--=====================================
 		CABECERA
 		======================================-->
-	<header class="container-fluid bg-primary">
-		<div class="container bg-danger">
+	<header class="container-fluid">
+		<div class="container p-0">
 			<div class="row">
 
 				<!-- LOGO -->
-				<div class="col-7 bg-success">
+				<div class="col-10 col-sm-11 col-md-8 col-lg-7 pt-1 pt-lg-2 p-xl-0">
 					<a href="index.php">
 						<img src="img/logotipo.png" alt="" class="img-fluid logotipo">
 					</a>
 				</div>
 
 				<!-- REDES SOCIALES -->
-				<div class="col-4 bg-warning redes">
-					<ul>
+				<div class="d-none d-md-block col-md-2 col-lg-3 redes">
+					<ul class="d-flex justify-content-end pt-2 mt-1">
 						<li>
 							<a href="#" target="_blank">
-								<i class="fab fa-facebook-f lead float-left rounded-circle text-white mr-1">
+								<i class="fab fa-facebook-f rounded-circle text-white mr-1">
 									
 								</i>
 							</a>
 						</li>
 						<li>
 							<a href="#" target="_blank">
-								<i class="fab fa-instagram lead float-left rounded-circle text-white mr-1">
+								<i class="fab fa-instagram rounded-circle text-white mr-1">
 									
 								</i>
 							</a>
 						</li>
 						<li>
 							<a href="#" target="_blank">
-								<i class="fab fa-twitter lead float-left rounded-circle text-white mr-1">
+								<i class="fab fa-twitter rounded-circle text-white mr-1">
 									
 								</i>
 							</a>
 						</li>
 						<li>
 							<a href="#" target="_blank">
-								<i class="fab fa-youtube lead float-left rounded-circle text-white mr-1">
+								<i class="fab fa-youtube rounded-circle text-white mr-1">
 									
 								</i>
 							</a>
@@ -85,14 +85,16 @@
 				</div>
 
 				<!-- BUSCADOR Y BOTÓN MENÚ -->
-				<div class="col-1 bg-danger">
+				<div class="col-2 col-sm-1 col-md-2 col-lg-2 d-flex justify-content-end pt-2 mt-1">
+
 					<!-- Buscador-->
-					<div class="float-left">
-						<i class="fas fa-search"></i>
+					<div class="d-none d-md-block pr-4 pr-lg-5 mt-1">
+						<i class="fas fa-search lead text-white"></i>
 					</div>
+
 					<!-- Botón Menú-->
-					<div class="float-left">
-						<i class="fas fa-bars"></i>
+					<div class="m-0 mt-sm-1 mt-md-0 pr-0 pr-sm-2 pr-lg-3">
+						<i class="fas fa-bars lead text-white"></i>
 					</div>
 				</div>
 
